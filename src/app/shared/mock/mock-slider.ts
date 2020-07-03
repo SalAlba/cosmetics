094d63bbd75602a5814ff0dfe40d9902edaca0ab
salalba <saljan93@live.com>
2020-07-03 13:43:37 +0200

    styles

--- src/app/shared/mock/mock-slider.ts
+++ src/app/shared/mock/mock-slider.ts
@@ -1,24 +1,49 @@
 import { Slide } from "../models/slide.model";
 
 export const SLIDES: Slide[] = [
+    // {
+    //     img: 'https://cosmetista.cmsmasters.net/wp-content/uploads/2018/02/home-1.jpg',
+    //     order: 1,
+    //     status: 'active',
+    //     hasDescription: true,
+    //     description: `
+    //         <div class="carousel-caption text-right">
+    //             <h1>Example headline.</h1>
+    //             <p>
+    //                 Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+    //             </p>
+    //             <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+    //         </div>
+    //     `,
+    //     visible: true
+    // },
+    // {
+    //     img: 'https://cosmetista.cmsmasters.net/wp-content/uploads/2017/11/home-12-1.jpg',
+    //     order: 2,
+    //     status: 'inactive',
+    //     hasDescription: false,
+    //     description: "",
+    //     visible: true
+    // },
+    // {
+    //     img: 'https://cosmetista.cmsmasters.net/wp-content/uploads/2017/11/slide2-1.jpg',
+    //     order: 3,
+    //     status: 'inactive',
+    //     hasDescription: false,
+    //     description: "",
+    //     visible: true
+    // }
+
     {
-        img: 'https://cosmetista.cmsmasters.net/wp-content/uploads/2018/02/home-1.jpg',
+        img: 'assets/images/sliders/JUVEDERM-WOMEN-HEADER.jpg',
         order: 1,
         status: 'active',
-        hasDescription: true,
-        description: `
-            <div class="carousel-caption text-right">
-                <h1>Example headline.</h1>
-                <p>
-                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                </p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-            </div>
-        `,
+        hasDescription: false,
+        description: "",
         visible: true
     },
     {
-        img: 'https://cosmetista.cmsmasters.net/wp-content/uploads/2017/11/home-12-1.jpg',
+        img: 'assets/images/sliders/Profhilo-header.jpg',
         order: 2,
         status: 'inactive',
         hasDescription: false,
@@ -26,7 +51,7 @@ export const SLIDES: Slide[] = [
         visible: true
     },
     {
-        img: 'https://cosmetista.cmsmasters.net/wp-content/uploads/2017/11/slide2-1.jpg',
+        img: 'assets/images/sliders/volbella-header.jpg',
         order: 3,
         status: 'inactive',
         hasDescription: false,
